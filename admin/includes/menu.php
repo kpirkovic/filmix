1d0983461412eba125846a5b2a41c03c699375c2
Kristijan Pirkovic <93116779+kpirkovic@users.noreply.github.com>
2022-09-06 13:52:08 +0200

    Removed an image

--- admin/includes/menu.php
+++ admin/includes/menu.php
@@ -4,7 +4,7 @@
         <li><a class='brand py-2 px-3 text-white text-decoration-none fs-2x5 logo' href="<?php echo DIRADMIN;?>"><?php echo SITETITLE;?></a></li>
         <div class='d-flex flex-column gap-3 mb-5'>
             <li><a class="text-decoration-none d-flex gap-3 align-items-center py-2 px-3 rounded-3 sidebar-btn text-white" href="<?php echo DIR;?>" target="_blank"><i class="bi bi-globe fs-6"></i><span>View Site</span></a></li>
-            <li><a class="text-decoration-none d-flex gap-3 align-items-center py-2 px-3 rounded-3 sidebar-btn <?php currentPage('pages'); ?>" href="<?php echo DIRADMIN;?>?page=pages"><i class="bi bi-file-earmark-plus"></i><span>Pages</span></a></li>
+            {# <li><a class="text-decoration-none d-flex gap-3 align-items-center py-2 px-3 rounded-3 sidebar-btn <?php currentPage('pages'); ?>" href="<?php echo DIRADMIN;?>?page=pages"><i class="bi bi-file-earmark-plus"></i><span>Pages</span></a></li> #}
             <li><a class="text-decoration-none d-flex gap-3 align-items-center py-2 px-3 rounded-3 sidebar-btn <?php currentPage('directors'); ?>" href="<?php echo DIRADMIN;?>?page=directors"><i class="bi bi-person-plus fs-5"></i><span>Directors</span></a></li>
             <li><a class="text-decoration-none d-flex gap-3 align-items-center py-2 px-3 rounded-3 sidebar-btn <?php currentPage('movies'); ?>" href="<?php echo DIRADMIN;?>?page=movies"><i class="bi bi-camera-reels fs-6"></i><span>Movies</span></a></li>
             <li><a class="text-decoration-none d-flex gap-3 align-items-center py-2 px-3 rounded-3 sidebar-btn <?php currentPage('genres'); ?>" href="<?php echo DIRADMIN;?>?page=genres"><i class="bi bi-film fs-6"></i><span>Genres</span></a></li>
