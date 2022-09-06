ce5822f7e5e34a3561fbc4dda853a44f33ec6094
Kristijan Pirkovic <93116779+kpirkovic@users.noreply.github.com>
2022-09-06 13:39:51 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -6,10 +6,10 @@ session_start();
 require_once('connection.php');
 
 // define site path
-define('DIR','https://filmix-movies.herokuapp.com/');
+define('DIR','https://filmix.herokuapp.com/');
 
 // define admin site path
-define('DIRADMIN','https://filmix-movies.herokuapp.com/admin/');
+define('DIRADMIN','https://filmix.herokuapp.com/admin/');
 
 // define site title for top of the browser
 define('SITETITLE','Filmx');
